932e2e7b6a9647804f3776ccc12fc081203e6a4c
Johan sundstrom <johansundstrom@users.noreply.github.com>
2018-04-19 15:49:57 +0200

    Create config.h
    init

---
+++ ds18b20_ntp_lcd/config.h
@@ -0,0 +1,6 @@
+const char *ssid     = "MdH-Guest";
+const char *password = "Houston201804";
+int DS18S20_pin = 18;   //ds18b20 --> ESP32(D18)
+int LCD_address = 0x3f; //I2C address (0x3f)
+int LCD_chars = 16;
+int LCD_rows = 2;
