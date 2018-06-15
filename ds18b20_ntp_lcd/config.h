d3a7390620e8088cbc71ab95c04deed7c30287f6
Johan sundstrom <johansundstrom@users.noreply.github.com>
2018-06-15 14:07:32 +0200

    Update config.h
    celcius

--- ds18b20_ntp_lcd/config.h
+++ ds18b20_ntp_lcd/config.h
@@ -4,3 +4,16 @@ int DS18S20_pin = 18;   //ds18b20 --> ESP32(D18)
 int LCD_address = 0x3f; //I2C address (0x3f)
 int LCD_chars = 16;
 int LCD_rows = 2;
+
+
+// Grader Celcius-tecken
+uint8_t centigrade[8] = {
+  B01000,
+  B10100,
+  B01000,
+  B00011,
+  B00100,
+  B00100,
+  B00011,
+  B00000
+};
